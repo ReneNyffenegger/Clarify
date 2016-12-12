99e6ec05446a8e80b5e254e53fa3f8b027cfbf4e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-12-12 09:51:01 +0100

    Hierarchy

--- tables/site_part.sql
+++ tables/site_part.sql
@@ -14,3 +14,31 @@ from
 --where
 --  prd.instance_name = '..'
 ;
+
+
+-- Hierarchy {
+
+select /*+ parallel(16) */
+   sp3.instance_name            sp3_instance_name,
+   sp2.instance_name            sp2_instance_name,
+   sp1.instance_name            sp1_instance_name,
+   pn3.description              pn3_description,
+   pn2.description              pn2_description,
+   pn1.description              pn1_description,
+   pn3.domain                   pn3_domain,
+   pn2.domain                   pn2_domain,
+   pn1.domain                   pn1_domain,
+   pn3.family                   pn3_family,
+   sp3.all_site_part2site       sp3_all_site_part2site
+from 
+  site_part sp3                                        join part_num pn3 on sp3.site_part2part_info = pn3.objid      join
+  site_part sp2 on sp2.objid = sp3.site_part2site_part join part_num pn2 on sp2.site_part2part_info = pn2.objid      join
+  site_part sp1 on sp1.objid = sp2.site_part2site_part join part_num pn1 on sp1.site_part2part_info = pn1.objid
+order by
+  sp3.all_site_part2site,
+  sp1.objid,
+  sp2.objid,
+  sp3.objid;
+
+
+-- }
