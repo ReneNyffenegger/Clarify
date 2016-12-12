f2d677c1b10081efb4bd4fc315c75950f08d9267
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-12-12 09:38:57 +0100

    Update demand_hdr.sql

--- tables/demand_hdr.sql
+++ tables/demand_hdr.sql
@@ -5,3 +5,18 @@ from
   contact    con on hdr.attention2contact = con.objid
 order by
   hdr.header_date;
+
+
+
+select
+  hdr.objid,
+  hdr.sequence_num, -- should indicate number of demand_dtl records for a demand_hdr record
+  count(*)
+from
+  demand_hdr hdr join
+  demand_dtl dtl on hdr.objid = dtl.demand_dtl2demand_hdr
+group by
+  hdr.objid,
+  hdr.sequence_num
+having
+  hdr.sequence_num <> count(*);
